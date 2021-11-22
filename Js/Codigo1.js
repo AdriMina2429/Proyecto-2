71184a08b00467d0111f2dbd8a762bbda6dcbf9f
Yanira <jmonge@estudiantes.crackthecode.la>
2021-11-22 12:28:34 -0500

    avance 2

--- Js/Codigo1.js
+++ Js/Codigo1.js
@@ -9,3 +9,24 @@ document.getElementById("btn-cambiar").addEventListener("click",function(){
     }
     document.getElementById("new_msj").innerHTML = nuevoMensaje;
    })
+   document.getElementById("btn-cambiar").addEventListener("click",function(){
+    var mensaje = document.getElementById("msj").value;
+    var nuevoMensaje = "";
+    for(i=0;i<mensaje.length ;i++){
+        var codMsj = mensaje[i].charCodeAt();
+        var codNuevo = codMsj + 2;
+        nuevoMensaje += String.fromCharCode(codNuevo);
+    }
+    document.getElementById("new_msj").innerHTML = nuevoMensaje;
+   })
+
+function bnt(){
+    var continuar = document.getElementById("ctnr").value;
+
+    alert("¡Bienvenid@ al lugar secreto de los teletubies!");
+}
+function enviar(){
+    var enviar = document.getElementById("enviar").value;
+    
+    alert("Gracias por pertenecer a la familia Teletubie <3");
+}
